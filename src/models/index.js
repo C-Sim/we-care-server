9260a5e416fa18d86b1d2518ce4c5e3a311ab887
Amelie Pira <amelie.pira@gmail.com>
2022-08-23 10:19:01 +0100

    added index.js for models

---
+++ src/models/index.js
@@ -0,0 +1,15 @@
+const User = require("./User");
+const Carer = require("./Carer");
+const Patient = require("./Patient");
+const Supervisor = require("./Supervisor");
+const Appointment = require("./Appointment");
+const Notification = require("./Notification");
+
+module.exports = {
+  User,
+  Appointment,
+  Carer,
+  Patient,
+  Supervisor,
+  Notification,
+};
